605655e4c7e6d62c4ceed6ed4d5c933019c2025b
Tensor-Programming <abeltensor@tensor-programming.com>
2019-03-15 21:46:00 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlareActor(
+          "assets/Solar.flr",
+          fit: BoxFit.cover,
+          alignment: Alignment.center,
+          animation: 'rotate',
+        ),
+      ),
+    );
+  }
+}
